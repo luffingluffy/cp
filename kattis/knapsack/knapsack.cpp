158c3b4c795015b51d0e4b1bab281d131c0ce670
Lutfi Randiva <lutfirandiva@outlook.com>
2022-10-24 17:39:52 +0800

    Rename Kattis files

---
+++ kattis/knapsack/knapsack.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main() {
+    int c, n;
+
+    while (cin >> c && cin >> n) {
+        vector<vi> object_list;
+        for (int i = 0; i < n; i++) {
+            vi object;
+            int v, w;
+            cin >> v >> w;
+            object.push_back(v);
+            object.push_back(w);
+        }
+
+    return 0;
+    }
+}
---
+++ kattis/turbo/turbo.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main() {
+    int n, e;
+    cin >> n;
+    vi v;
+
+    for (int i = 0; i < n; i++) {
+        cin >> e;
+        v.push_back(e);
+    }
+
+    return 0;
+}
