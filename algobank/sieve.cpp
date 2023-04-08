c0f86148c49ee1667a03c245bbc4ab3751dfdda4
Lutfi Randiva <lutfirandiva@outlook.com>
2023-04-09 00:30:12 +0800

    Update sieve.cpp

--- algobank/sieve.cpp
+++ algobank/sieve.cpp
@@ -1,15 +1,32 @@
+#include <vector>
+using namespace std;
+
 vector<int> primes;
+
 void sieve(int n) {
-    vi isPrime(n + 1, 1);
-    for (int i = 2; i <= N; i++) {
-        if (isPrime[i]) {
+    // Create a boolean vector to represent prime candidates up to n
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = false;
+    is_prime[1] = false;
+
+    // Iterate over the numbers from 2 to n
+    for (int i = 2; i <= n; i++) {
+        // If the current number is marked as prime, add it to the list of primes
+        if (is_prime[i]) {
             primes.push_back(i);
         }
+
+        // Iterate over the already-found primes
         for (auto p : primes) {
-            if (i * p > N) {
+            // If the product of the current number and prime exceeds n, stop the inner loop
+            if (i * p > n) {
                 break;
             }
-            isPrime[i * p] = 0;
+
+            // Mark the product of the current number and prime as not prime
+            is_prime[i * p] = false;
+
+            // If the current number is divisible by the prime, stop the inner loop
             if (i % p == 0) {
                 break;
             }
